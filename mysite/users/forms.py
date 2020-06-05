7189b7b0b51888cf429442eaf1edd89189d6c94b
Sammype <66485350+Sammype@users.noreply.github.com>
2020-06-05 11:31:22 -0500

    Update forms.py

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
@@ -10,8 +11,18 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+def ensure_endswith(input):
+    for number in range(1):
+        if input.endswith('@drexel.edu'):
+            break
+        else:
+            input = forms.EmailField()
+            print('You must use an email that ends with @drexel.edu')
+        
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
+    ensure_endswith(email)
+        
 
     class Meta:
         model = User
